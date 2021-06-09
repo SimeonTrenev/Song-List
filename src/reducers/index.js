5f0a7ae61d68584127c867f435e5267f5ded862d
Simeon Trenev <65025840+SimeonTrenev@users.noreply.github.com>
2021-06-09 18:33:10 +0300

    commit

---
+++ src/reducers/index.js
@@ -0,0 +1,23 @@
+import { combineReducers } from 'redux';
+
+const songsReducer = () => {
+    return [
+        { title: 'No Scrubs', duration: '4:05' },
+        { title: 'Macarena', duration: '2:30' },
+        { title: 'All Star', duration: '3:15' },
+        { title: 'I Want it That Way', duration: '2:59' }
+    ];
+};
+
+const selectedSongReducer = (selectedSong = null, action) => {
+    if(action.type === 'SONG_SELECTED') {
+        return action.payload;
+    }
+
+    return selectedSong;
+};
+
+export default combineReducers({
+    songs: songsReducer,
+    selectedSong: selectedSongReducer
+})
